8231699cb2ab9f0e9520f8d85676099b766e42a1
rono-007 <iemrono2022@gmail.com>
2026-08-31 21:31:01 +0530

    feat: integrate Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import RootErrorBoundary from './components/RootErrorBoundary'
@@ -28,6 +29,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RootErrorBoundary>
       <App />
+      <Analytics />
     </RootErrorBoundary>
   </StrictMode>,
 )
